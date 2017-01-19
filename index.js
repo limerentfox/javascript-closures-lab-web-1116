f76e4228166b5ca6f25dadce2c696a89426ed940
limerentfox <christi@limerentfox.com>
2017-01-18 21:45:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter ++
+    return counter
+  }
+
+  function getBumps() {
+    var currentCounter = counter
+    return currentCounter
+  }
+
+  return {addBump, getBumps}
+
+}
+
+function createAnimal(animalType) {
+  return function(deadlyDevice) {
+    return {animalType, deadlyDevice}
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
